0f9962e8bfa1bf33234928177a884d0aaf12b543
David <davidkimighty@gmail.com>
2022-08-30 17:50:34 +0900

    Init project

---
+++ Runtime/UI/BaseInteractable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollieMollie.UI
+{
+    public class BaseInteractable : MonoBehaviour
+    {
+
+    }
+}
