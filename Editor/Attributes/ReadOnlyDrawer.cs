96e9bd8c9a15eb4e34cf37f81728f0e0904d8946
David <davidkimighty@gmail.com>
2022-09-12 10:30:33 +0900

    Added readonly attribute

---
+++ Editor/Attributes/ReadOnlyDrawer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CollieMollie.Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool previousGUIState = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = previousGUIState;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = true)]
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
