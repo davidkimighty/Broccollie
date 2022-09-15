272df6f6e8885603f27ab607cbddea41346cc58c
David <davidkimighty@gmail.com>
2022-09-15 15:14:08 +0900

    Updated helpers

--- Runtime/Helper/HelperMath.cs
+++ Runtime/Helper/HelperMath.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ namespace CollieMollie.Helper
     public static partial class Helper
     {
         #region Vector3
-        public static IEnumerator LerpPosition(this Transform transform, Vector3 targetPosition, float duration)
+        public static IEnumerator LerpPosition(this Transform transform, Vector3 targetPosition, float duration, Action done = null)
         {
             float elapsedTime = 0f;
             Vector3 startingPosition = transform.position;
@@ -20,9 +21,26 @@ namespace CollieMollie.Helper
                 yield return null;
             }
             transform.position = targetPosition;
+            done?.Invoke();
         }
 
-        public static IEnumerator LerpLocalPosition(this Transform transform, Vector3 targetPosition, float duration)
+        public static IEnumerator LerpPosition(this Transform transform, Vector3 targetPosition, float duration, AnimationCurve curve, Action done = null)
+        {
+            float elapsedTime = 0f;
+            Vector3 startingPosition = transform.position;
+
+            while (elapsedTime < duration)
+            {
+                Vector3 lerpValue = Vector3.Lerp(startingPosition, targetPosition, curve.Evaluate(elapsedTime / duration));
+                transform.position = lerpValue;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = targetPosition;
+            done?.Invoke();
+        }
+
+        public static IEnumerator LerpLocalPosition(this Transform transform, Vector3 targetPosition, float duration, Action done = null)
         {
             float elapsedTime = 0f;
             Vector3 startingPosition = transform.localPosition;
@@ -35,11 +53,28 @@ namespace CollieMollie.Helper
                 yield return null;
             }
             transform.localPosition = targetPosition;
+            done?.Invoke();
+        }
+
+        public static IEnumerator LerpLocalPosition(this Transform transform, Vector3 targetPosition, float duration, AnimationCurve curve, Action done = null)
+        {
+            float elapsedTime = 0f;
+            Vector3 startingPosition = transform.localPosition;
+
+            while (elapsedTime < duration)
+            {
+                Vector3 lerpValue = Vector3.Lerp(startingPosition, targetPosition, curve.Evaluate(elapsedTime / duration));
+                transform.localPosition = lerpValue;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localPosition = targetPosition;
+            done?.Invoke();
         }
         #endregion
 
         #region Quaternion
-        public static IEnumerator LerpRotation(this Transform transform, Quaternion targetRotation, float duration)
+        public static IEnumerator LerpRotation(this Transform transform, Quaternion targetRotation, float duration, Action done = null)
         {
             float elapsedTime = 0f;
             Quaternion startingRotation = transform.rotation;
@@ -52,9 +87,26 @@ namespace CollieMollie.Helper
                 yield return null;
             }
             transform.rotation = targetRotation;
+            done?.Invoke();
         }
 
-        public static IEnumerator LerpLocalRotation(this Transform transform, Quaternion targetRotation, float duration)
+        public static IEnumerator LerpRotation(this Transform transform, Quaternion targetRotation, float duration, AnimationCurve curve, Action done = null)
+        {
+            float elapsedTime = 0f;
+            Quaternion startingRotation = transform.rotation;
+
+            while (elapsedTime < duration)
+            {
+                Quaternion lerpValue = Quaternion.Lerp(startingRotation, targetRotation, curve.Evaluate(elapsedTime / duration));
+                transform.rotation = lerpValue;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.rotation = targetRotation;
+            done?.Invoke();
+        }
+
+        public static IEnumerator LerpLocalRotation(this Transform transform, Quaternion targetRotation, float duration, Action done = null)
         {
             float elapsedTime = 0f;
             Quaternion startingRotation = transform.localRotation;
@@ -67,6 +119,23 @@ namespace CollieMollie.Helper
                 yield return null;
             }
             transform.localRotation = targetRotation;
+            done?.Invoke();
+        }
+
+        public static IEnumerator LerpLocalRotation(this Transform transform, Quaternion targetRotation, float duration, AnimationCurve curve, Action done = null)
+        {
+            float elapsedTime = 0f;
+            Quaternion startingRotation = transform.localRotation;
+
+            while (elapsedTime < duration)
+            {
+                Quaternion lerpValue = Quaternion.Lerp(startingRotation, targetRotation, curve.Evaluate(elapsedTime / duration));
+                transform.localRotation = lerpValue;
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localRotation = targetRotation;
+            done?.Invoke();
         }
         #endregion
     }
