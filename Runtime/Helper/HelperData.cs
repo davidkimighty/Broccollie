2d2000d9917409fffcba00fa49f627b5d233eaa0
David <davidkimighty@gmail.com>
2022-10-07 10:00:46 +0900

    Added json helper

---
+++ Runtime/Helper/HelperData.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollieMollie.Helper
+{
+    public static partial class Helper
+    {
+        #region Json
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        public static string FixJsonFromServer(string value)
+        {
+            return "{\"Items\":" + value + "}";
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+        #endregion
+    }
+}
