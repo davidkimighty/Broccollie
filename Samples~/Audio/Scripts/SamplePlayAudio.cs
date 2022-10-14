f4994ba16d879f32a91ee234a71bbbab4378a324
David <davidkimighty@gmail.com>
2022-10-14 17:37:58 +0900

    AudioManager background volume change

--- Samples~/Audio/Scripts/SamplePlayAudio.cs
+++ Samples~/Audio/Scripts/SamplePlayAudio.cs
@@ -1,15 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using CollieMollie.Audio;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SamplePlayAudio : MonoBehaviour
 {
-    [SerializeField] private AudioEventChannel eventChannel = null;
-    [SerializeField] private AudioPreset preset = null;
+    private const string SFX = "SFXVolume";
+    private const string AMBIENT = "AmbientVolume";
 
-    public void PlaySampleAudio()
+    [SerializeField] private AudioMixer _masterAudioMixer = null;
+
+    [SerializeField] private Slider _ambientSlider = null;
+
+    [SerializeField] private AudioEventChannel _eventChannel = null;
+    [SerializeField] private AudioPreset _backgroundAudioPreset = null;
+    [SerializeField] private AudioPreset _buttonAudioPreset = null;
+
+    private void Start()
     {
-        eventChannel.RaisePlayAudioEvent(preset);
+        _eventChannel.RaisePlayAudioEvent(_backgroundAudioPreset);
+    }
+
+    public void PlayButtonAudio()
+    {
+        _eventChannel.RaisePlayAudioEvent(_buttonAudioPreset);
+    }
+
+    public void ChangeVolume(float value)
+    {
+        _ambientSlider.value = value;
+        _masterAudioMixer.SetFloat(AMBIENT, Mathf.Log10(value) * 20);
     }
 }
