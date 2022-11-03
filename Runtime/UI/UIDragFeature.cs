7fed37a0a124488eefb8f666c75f4d4bd3c0f59f
David <davidkimighty@gmail.com>
2022-11-03 16:13:59 +0900

    UI drag feature

--- Runtime/UI/UIDragFeature.cs
+++ Runtime/UI/UIDragFeature.cs
@@ -15,14 +15,26 @@ namespace CollieMollie.UI
         [SerializeField] private UIDragPreset _preset = null;
 
         private bool _dragging = false;
-        private Vector2 _lastPosition = Vector2.zero;
+        private Vector3 _lastPosition = Vector2.zero;
         private IEnumerator _dragAction = null;
         #endregion
 
         #region Public Functions
         public void Drag(PointerEventData eventData)
         {
-            _lastPosition = eventData.position;
+            switch (_dragArea.renderMode)
+            {
+                case RenderMode.ScreenSpaceOverlay:
+                    _lastPosition = eventData.position;
+                    break;
+
+                case RenderMode.ScreenSpaceCamera:
+                    Vector3 point = new Vector3(eventData.position.x, eventData.position.y, _dragArea.planeDistance);
+                    Vector3 screen2World = Camera.main.ScreenToWorldPoint(point);
+                    screen2World.z = _dragArea.planeDistance;
+                    _lastPosition = screen2World;
+                    break;
+            }
 
             if (_dragAction == null)
             {
@@ -53,7 +65,7 @@ namespace CollieMollie.UI
                 }
                 else
                 {
-                    _dragTarget.position = Vector2.Lerp(_dragTarget.position, _lastPosition, _preset.DragSpeed);
+                    _dragTarget.position = Vector3.Lerp(_dragTarget.position, _lastPosition, _preset.DragSpeed);
                     yield return null;
                 }
             }
