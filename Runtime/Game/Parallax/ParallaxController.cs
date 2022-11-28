0ac715c48b6dfde1d6c3145f0cd5299c477ba27e
David <davidkimighty@gmail.com>
2022-11-28 16:33:07 +0900

    Parallax effect controller. close #19

---
+++ Runtime/Game/Parallax/ParallaxController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CollieMollie.Game
+{
+    public class ParallaxController : MonoBehaviour
+    {
+        #region Variable Field
+        [SerializeField] private float _fixedParallaxSpeed = 3f;
+        [SerializeField] private float _minParallaxSpeed = 3f;
+        [SerializeField] private float _maxParallaxSpeed = 8f;
+        [SerializeField] private BackgroundLayer[] _layers = null;
+        [SerializeField] private Material _material = null;
+        [SerializeField] private AnimationCurve _parallaxCurve = null;
+
+        private IEnumerator _parallaxAction = null;
+
+        #endregion
+
+        private void Awake()
+        {
+            for (int i = 0; i < _layers.Length; i++)
+                _layers[i].Background.material = new Material(_material);
+
+        }
+
+        #region Public Functions
+        public void StartParallax(int dir, float duration, Action done = null)
+        {
+            if (_parallaxAction != null)
+                StopCoroutine(_parallaxAction);
+
+            _parallaxAction = Parallax(dir, duration, _parallaxCurve, done);
+            StartCoroutine(_parallaxAction);
+        }
+
+        public void StartParallaxLoop(int dir)
+        {
+            if (_parallaxAction != null)
+                StopCoroutine(_parallaxAction);
+
+            _parallaxAction = ParallaxLoop(dir);
+            StartCoroutine(_parallaxAction);
+        }
+
+        public void StopParallax()
+        {
+            if (_parallaxAction != null)
+                StopCoroutine(_parallaxAction);
+        }
+
+        #endregion
+
+        #region Private Functions
+        private IEnumerator Parallax(int dir, float duration, AnimationCurve curve, Action done = null)
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                float lerpSpeed = Mathf.Lerp(_minParallaxSpeed, _maxParallaxSpeed, curve.Evaluate(elapsedTime / duration));
+                for (int i = 0; i < _layers.Length; i++)
+                {
+                    Vector2 offset = _layers[i].Background.materialForRendering.mainTextureOffset;
+                    offset.x += dir * (lerpSpeed * Time.deltaTime) * _layers[i].ParallaxValue;
+                    _layers[i].Background.materialForRendering.mainTextureOffset = offset;
+                }
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            done?.Invoke();
+        }
+
+        private IEnumerator ParallaxLoop(int dir)
+        {
+            while (true)
+            {
+                for (int i = 0; i < _layers.Length; i++)
+                {
+                    Vector2 offset = _layers[i].Background.materialForRendering.mainTextureOffset;
+                    offset.x += dir * (_fixedParallaxSpeed * Time.deltaTime) * _layers[i].ParallaxValue;
+                    _layers[i].Background.materialForRendering.mainTextureOffset = offset;
+                }
+                yield return null;
+            }
+        }
+        #endregion
+    }
+
+    [Serializable]
+    public class BackgroundLayer
+    {
+        public Image Background = null;
+        public float ParallaxValue = 0.1f;
+    }
+}
---
+++ Samples~/Game/Parallax/Scripts/SampleParallaxController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using CollieMollie.Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleParallaxController : MonoBehaviour
+{
+    [SerializeField] private ParallaxController _parallaxController = null;
+    [SerializeField] private Button _buttonLeft = null;
+    [SerializeField] private Button _buttonRight = null;
+    [SerializeField] private Button _buttonStart = null;
+    [SerializeField] private Button _buttonStop = null;
+    [SerializeField] private float _duration = 3f;
+
+    private void Awake()
+    {
+        _buttonLeft.onClick.AddListener(() => _parallaxController.StartParallax(-1, _duration));
+        _buttonRight.onClick.AddListener(() => _parallaxController.StartParallax(+1, _duration));
+        _buttonStart.onClick.AddListener(() => _parallaxController.StartParallaxLoop(+1));
+        _buttonStop.onClick.AddListener(() => _parallaxController.StopParallax());
+    }
+}
