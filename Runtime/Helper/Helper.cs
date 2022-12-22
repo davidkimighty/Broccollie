175d0d055e409d92cdf5dda322aa6a1a1a25db4c
David <davidkimighty@gmail.com>
2022-12-22 23:25:40 +0900

    camera viewport helper

--- Runtime/Helper/Helper.cs
+++ Runtime/Helper/Helper.cs
@@ -17,6 +17,35 @@ namespace CollieMollie.Helper
         }
         #endregion
 
+        #region Camera Viewport
+        public static Vector3 ViewportToWorldPlanePoint(this Camera cam, float zDepth, Vector2 viewportCord)
+        {
+            Vector2 angles = cam.ViewportPointToAngle(viewportCord);
+            float xOffset = Mathf.Tan(angles.x) * zDepth;
+            float yOffset = Mathf.Tan(angles.y) * zDepth;
+            Vector3 cameraPlanePosition = new Vector3(xOffset, yOffset, zDepth);
+            cameraPlanePosition = cam.transform.TransformPoint(cameraPlanePosition);
+            return cameraPlanePosition;
+        }
+
+        public static Vector2 ViewportPointToAngle(this Camera cam, Vector2 ViewportCord)
+        {
+            float adjustedAngle = AngleProportion(cam.fieldOfView / 2, cam.aspect) * 2;
+            float xProportion = (ViewportCord.x - 0.5f) / 0.5f;
+            float yProportion = (ViewportCord.y - 0.5f) / 0.5f;
+            float xAngle = AngleProportion(adjustedAngle / 2, xProportion) * Mathf.Deg2Rad;
+            float yAngle = AngleProportion(cam.fieldOfView / 2, yProportion) * Mathf.Deg2Rad;
+            return new Vector2(xAngle, yAngle);
+        }
+
+        public static float AngleProportion(float angle, float proportion)
+        {
+            float oppisite = Mathf.Tan(angle * Mathf.Deg2Rad);
+            float oppisiteProportion = oppisite * proportion;
+            return Mathf.Atan(oppisiteProportion) * Mathf.Rad2Deg;
+        }
+        #endregion
+
         #region Diagnostics
         public static Stopwatch StartWatch()
         {
