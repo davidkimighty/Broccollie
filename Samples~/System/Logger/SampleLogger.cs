74d88c5b49fd3e71f084cdfd8e9dbf22dcc88ca3
David <davidkimighty@gmail.com>
2023-01-08 17:13:44 +0900

    #9 logger sample

---
+++ Samples~/System/Logger/SampleLogger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using CollieMollie.Helper;
+using UnityEngine;
+
+public class SampleLogger : MonoBehaviour
+{
+    private void Start()
+    {
+        Helper.Log("This is Log", Helper.Broccollie, this);
+        Helper.LogWarning("This is LogWarning", Helper.Broccollie, this);
+        Helper.LogError("This is LogError", Helper.Broccollie, this);
+    }
+}
