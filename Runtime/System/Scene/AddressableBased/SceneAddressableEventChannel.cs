8d3fb66cef1e923ca8f5058142f2e121bc80f11d
David <davidkimighty@gmail.com>
2023-02-14 11:42:28 +0900

    #45 close - adding scene addressable event channel

---
+++ Runtime/System/Scene/AddressableBased/SceneAddressableEventChannel.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace CollieMollie.System
+{
+    [CreateAssetMenu(fileName = "EventChannel_SceneAddressable", menuName = "CollieMollie/Event Channels/Scene Addressable")]
+    public class SceneAddressableEventChannel : ScriptableObject
+    {
+        #region Events
+        public event Action<SceneAddressablePreset> OnSceneLoadRequest = null;
+
+        #endregion
+
+        #region Publishers
+        public void RaiseSceneLoadEvent(SceneAddressablePreset scene)
+        {
+            if (scene == null) return;
+            OnSceneLoadRequest?.Invoke(scene);
+        }
+
+        #endregion
+    }
+}
