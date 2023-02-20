ef6ea808ad9c1eb099ef1c4fcc7120dca5830cd8
David <davidkimighty@gmail.com>
2023-02-20 13:06:30 +0900

    Adding a simple fader along with a sample

--- Runtime/System/Scene/AddressableBased/SceneAddressableEventChannel.cs
+++ Runtime/System/Scene/AddressableBased/SceneAddressableEventChannel.cs
@@ -1,17 +1,26 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace CollieMollie.System
 {
-    [CreateAssetMenu(fileName = "EventChannel_SceneAddressable", menuName = "CollieMollie/Event Channels/Scene Addressable")]
+    [CreateAssetMenu(fileName = "EventChannel_SceneAddressable", menuName = "CollieMollie/EventChannels/Scene Addressable")]
     public class SceneAddressableEventChannel : ScriptableObject
     {
         #region Events
+        public event Func<SceneAddressablePreset, bool, Task> OnSceneLoadRequestAsync = null;
+
         public event Action<SceneAddressablePreset, bool> OnSceneLoadRequest = null;
 
         #endregion
 
         #region Publishers
+        public async Task RaiseSceneLoadEventAsync(SceneAddressablePreset scene, bool showLoadingScene)
+        {
+            if (scene == null) return;
+            await OnSceneLoadRequestAsync?.Invoke(scene, showLoadingScene);
+        }
+
         public void RaiseSceneLoadEvent(SceneAddressablePreset scene, bool showLoadingScene)
         {
             if (scene == null) return;
