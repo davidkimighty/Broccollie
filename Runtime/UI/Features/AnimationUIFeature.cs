5ec1fc6a6e54551507b2f127be15804f74b1864d
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-06-20 19:47:54 +0900

    removing editor script

--- Runtime/UI/Features/AnimationUIFeature.cs
+++ Runtime/UI/Features/AnimationUIFeature.cs
@@ -3,7 +3,6 @@ using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using UnityEditor.Presets;
 using UnityEngine;
 
 namespace Broccollie.UI
