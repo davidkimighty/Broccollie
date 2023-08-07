2b1c853bf66b06d08a546f3112c62bdeca8176b7
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-08-07 20:04:09 +0900

    adding event channel for simple fader

---
+++ Runtime/System/Scene/Fader/FaderEventChannel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Broccollie.System
+{
+    [CreateAssetMenu(fileName = "EventChannel_Fader", menuName = "Broccollie/Event Channels/Fader")]
+    public class FaderEventChannel : ScriptableObject
+    {
+        public event Action<float> OnFade = null;
+        public event Func<float, Task> OnFadeAsync = null;
+
+        #region Publishers
+        public void RequestFade(float alpha)
+        {
+            OnFade?.Invoke(alpha);
+        }
+
+        public async Task RequestFadeAsync(float alpha)
+        {
+            if (OnFadeAsync != null)
+                await OnFadeAsync.Invoke(alpha);
+        }
+
+        #endregion
+    }
+}
