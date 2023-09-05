3553c458109a1c2f4cd51e56c9aa1a25c39a4b28
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-09-05 14:22:52 +0900

    separating packages

--- Samples~/Game/CameraEffectURP/Runtime/Scripts/CameraEffectPreset.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Broccollie.Game.CameraEffect
-{
-    public abstract class CameraEffectPreset : ScriptableObject
-    {
-        public virtual void Play(MonoBehaviour mono, Transform camera) { }
-    }
-}
